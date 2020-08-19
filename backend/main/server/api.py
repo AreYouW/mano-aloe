a6d79c6ee147808d6ca5c0703ff74202ddb833d0
tduong <duongt.sbc@gmail.com>
2020-08-19 16:28:46 -0700

    Add messageID range for Rest API

--- backend/main/server/api.py
+++ backend/main/server/api.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_restful import Api
 
 
-from main.server.resources.Message import MessageListResource, MessageResource
+from main.server.resources.Message import MessageListResource, MessageResource, MessageListRangeResource
 
 # Api Blueprint
 api_bp = Blueprint('api', __name__)
@@ -12,3 +12,4 @@ api = Api(api_bp)
 
 api.add_resource(MessageListResource, '/messages')
 api.add_resource(MessageResource, '/messages/<messageID>')
+api.add_resource(MessageListRangeResource, '/messages/range/<lower>/<upper>')
