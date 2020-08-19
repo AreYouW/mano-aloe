918c62b9841b18fa54c1751fe74040e3fedd1aaf
AreYouW <44926928+areyouw@users.noreply.github.com>
2020-08-19 03:15:28 +0100

    Added english translation field to message model

--- backend/main/server/models.py
+++ backend/main/server/models.py
@@ -6,13 +6,15 @@ class Message(db.Model):
     __tablename__ = 'MESSAGES'
     messageID = db.Column(db.Integer, primary_key=True, autoincrement=True)
     original_message = db.Column(db.String(512), nullable=False)
-    translated_message = db.Column(db.String(512), nullable=True)
+    translated_japanese_message = db.Column(db.String(512), nullable=True)
+    translated_english_message = db.Column(db.String(512), nullable=True)
     region = db.Column(db.String(16), nullable=False)
     username = db.Column(db.String(256), nullable=False)
 
-    def __init__(self, original_message, translated_message, region, username):
+    def __init__(self, original_message, japanese_message, english_message, region, username):
         self.original_message = original_message
-        self.translated_message = translated_message
+        self.translated_japanese_message = japanese_message
+        self.translated_english_message = english_message
         self.region = region
         self.username = username
 
@@ -20,6 +22,7 @@ class Message(db.Model):
 class MessageSchema(ma.Schema):
     messageID = fields.Integer()
     original_message = fields.String(required=True)
-    translated_message = fields.String(required=False)
+    translated_japanese_message = fields.String(required=False)
+    translated_english_message = fields.String(required=False)
     region = fields.String(required=True)
     username = fields.String(required=True)
