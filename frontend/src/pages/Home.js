c46e5bb1cdc4ef054885e6a545871818dcacc85a
Timothy Shee <timothy_shee@hotmail.com>
2020-08-20 01:24:43 -0400

    Adding CardList (has styling, language support)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import MessageCardSection from '../components/MessageCardSection'
+import data from '../assets/messages.json'
 
 export default function HomePage() {
   return (
     <div>
       <h2>HOME</h2>
+      <MessageCardSection data={data} />
     </div>
   );
 }
