fadcc18679f2b9a4f302bff31015133b5f74e3fb
Timothy Shee <timothy_shee@hotmail.com>
2020-08-20 21:09:38 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,11 +7,9 @@ import data from '../assets/messages.json'
 export default class HomePage extends React.Component {
   render() {
     return (
-        <div>
-          <Container className="wrapperOverlay">
-            <MessageCardSection data={data} />
-          </Container>
-        </div>
+      <Container className="wrapper-overlay">
+        <MessageCardSection data={data} />
+      </Container>
     )
   }
 }
