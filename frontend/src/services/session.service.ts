bf5688a7816d3f374591b4975bb5d1e253592f0c
pineapple69 <masterofpudding@hotmail.com>
2020-08-22 13:21:20 +0200

    frontend cache

--- frontend/src/services/session.service.ts
+++ frontend/src/services/session.service.ts
@@ -2,20 +2,13 @@ import {Message} from "../models/message";
 
 export default class SessionService {
     public static saveMessages(messages: Message[]): void {
-        const messagesString = messages.map((message: Message) => {
-            return JSON.stringify(message);
-        })
-        localStorage.setItem('messages', JSON.stringify(messagesString));
+        localStorage.setItem('messages', JSON.stringify(messages));
     }
 
     public static getMessages(): Message[] {
         const messageString = localStorage.getItem('messages');
         if (messageString) {
-            const messagesString: string[] = JSON.parse(messageString);
-            const messages: Message[] = messagesString.map((message: string) => {
-                return JSON.parse(message) as Message;
-            })
-            return messages;
+            return JSON.parse(messageString) as Message[];
         }
         return [];
     }
