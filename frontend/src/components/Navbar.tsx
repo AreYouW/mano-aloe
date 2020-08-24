a5c2f38fc172f479e6f933b8395e19143ae39648
Nephren68 <65450159+Nephren68@users.noreply.github.com>
2020-08-24 14:57:53 +0800

    Fix warning

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button'
 
