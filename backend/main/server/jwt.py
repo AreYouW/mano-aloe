dfc07967b95bd33f524b9ba67f3a6a5104f60731
AreYouW <44926928+areyouw@users.noreply.github.com>
2020-08-26 04:42:56 +0100

    Allow tokens to be valid for a longer period of time

--- backend/main/server/jwt.py
+++ backend/main/server/jwt.py
@@ -2,8 +2,7 @@ from flask_jwt import JWT
 from main.server import app
 from main.server.models import User
 from passlib.hash import pbkdf2_sha256
-from os import environ
-
+from datetime import timedelta
 
 def authenticate(username, password):
     user = User.query.filter_by(username=username).first()
@@ -17,4 +16,8 @@ def identity(payload):
 
 
 app.config['SECRET_KEY'] = 'change_this_on_live'  # will change on upload
+
+# Allows tokens to be valid for longer
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=60 * 15)
+
 jwt = JWT(app, authenticate, identity)
