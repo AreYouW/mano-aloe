69e538bdff4315e1f62552dd8eaa9d11d4b4bbc9
AreYouW <44926928+areyouw@users.noreply.github.com>
2020-08-26 04:39:07 +0100

    Enforce PEP8

--- backend/main/server/resources/Gallery.py
+++ backend/main/server/resources/Gallery.py
@@ -6,19 +6,23 @@ from main.server.models import Gallery, GallerySchema
 artwork_schema = GallerySchema()
 gallery_schema = GallerySchema(many=True)
 
+
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     response.headers['Access-Control-Allow-Methods'] = 'GET,HEAD,OPTIONS,POST,PUT'
-    response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    response.headers[
+        'Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
     return response
+
 
 class GalleryCount(Resource):
     @cache.cached(timeout=100)
     def get(self):
         """Gets the number of messages available on the server"""
         return {'status': 'success', 'count': Gallery.query.count()}, 200
+
 
 class GalleryListResource(Resource):
     @cache.cached(timeout=100)
@@ -28,7 +32,8 @@ class GalleryListResource(Resource):
         gallery = gallery_schema.dump(gallery)
 
         if not gallery:
-            return {'status': 'success', 'gallery': gallery}, 206 #Partial Content Served, the other status code never loads
+            return {'status': 'success',
+                    'gallery': gallery}, 206  # Partial Content Served, the other status code never loads
 
         return {'status': 'success', 'gallery': gallery}, 200
 
@@ -60,4 +65,3 @@ class GalleryListResource(Resource):
         db.session.commit()
 
         return {'status': 'success', 'message': 'Artwork entry successfully created'}, 201
-
--- backend/main/server/resources/Game.py
+++ backend/main/server/resources/Game.py
@@ -6,13 +6,16 @@ from main.server.models import Games, GameSchema
 games_schema = GameSchema(many=True)
 game_schema = GameSchema()
 
+
 @app.after_request
 def add_header(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     response.headers['Access-Control-Allow-Methods'] = 'GET,HEAD,OPTIONS,POST,PUT'
-    response.headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    response.headers[
+        'Access-Control-Allow-Headers'] = 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
     return response
+
 
 class GameListResource(Resource):
     @cache.cached(timeout=100)
@@ -20,12 +23,11 @@ class GameListResource(Resource):
         """Gets all Artwork on the server"""
         games = Games.query.all()
         games = games_schema.dump(games)
-        
+
         if not games:
-            return {'status': 'success', 'games': games}, 206 #Partial Content Served
+            return {'status': 'success', 'games': games}, 206  # Partial Content Served
 
         return {'status': 'success', 'games': games}, 200
-
 
     def post(self):
         """Add Game"""
@@ -47,9 +49,9 @@ class GameListResource(Resource):
             return {'status': 'fail', 'message': 'Game already exists'}, 400
 
         message = Games(gameLink=data.get('gameLink'),
-                          gitLink=data.get('gitLink'),
-                          description=data.get('description'),
-                          title=data.get('title'))
+                        gitLink=data.get('gitLink'),
+                        description=data.get('description'),
+                        title=data.get('title'))
 
         db.session.add(message)
         db.session.commit()
@@ -62,4 +64,3 @@ class GameCount(Resource):
     def get(self):
         """Gets the number of games available on the server"""
         return {'status': 'success', 'count': Games.query.count()}, 200
-
