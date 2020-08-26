0c6856a07bb33e95ea6a7404109bb085235aca30
Nephren68 <65450159+Nephren68@users.noreply.github.com>
2020-08-26 16:28:01 +0800

    Add gallery section and artwork card components

---
+++ frontend/src/components/gallerySection/artworkCard/artworkCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './artworkCard.css';
+
+interface ArtworkCardProps {
+
+}
+
+interface ArtworkCardState {
+
+}
+
+export default class ArtworkCard extends React.Component<ArtworkCardProps, ArtworkCardState> {
+    constructor(props: ArtworkCardProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="artwork-card">
+                
+            </div>
+        )
+    }
+}
---
+++ frontend/src/components/gallerySection/gallerySection.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Artwork } from '../../models/artwork';
+import ArtworkCard from './artworkCard/artworkCard';
+import './gallerySection.css';
+
+interface GallerySectionProps {
+    data: Artwork[],
+}
+
+interface GallerySectionState {
+
+}
+
+export default class GallerySection extends React.Component<GallerySectionProps, GallerySectionState> {
+    private data: Artwork[];
+
+    constructor(props: GallerySectionProps) {
+        super(props);
+        this.data = props.data;
+    }
+
+    render() {
+        return (
+            <div className="gallery-section">
+                {this.data.map((artwork: Artwork) =>
+                    <ArtworkCard key={artwork.artworkID} />
+                )}
+            </div>
+        )
+    }
+}
