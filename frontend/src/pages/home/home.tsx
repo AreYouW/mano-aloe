0997639922ec6c716a0041b57613eed3172e9552
tduong <duongt.sbc@gmail.com>
2020-08-27 14:25:46 -0700

    fixed navbar on mobile devices

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -76,7 +76,9 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
                 </section>
 								<div className="notice-center">
 									<div className="notice-container">
-										<div className="notice-content">Those were all the messages we managed to collect, but there were many more sent your way! Please check <a href="https://twitter.com/hashtag/%E3%82%A2%E3%83%AD%E3%82%A8Worldwide?src=hashtag">#アロエWorldwide</a> and <a href="https://twitter.com/hashtag/Global%E3%82%A2%E3%83%AD%E3%82%A8?src=hashtag">#Globalアロエ</a> on Twitter, or visit <a href="https://manoaloe.jetri.co/">dragonjet</a>'s site for the rest!</div>
+										<div className="notice-content">
+											Those were all the messages we managed to collect, but there were many more sent your way! Please check <a href="https://twitter.com/hashtag/%E3%82%A2%E3%83%AD%E3%82%A8Worldwide?src=hashtag">#アロエWorldwide</a> and <a href="https://twitter.com/hashtag/Global%E3%82%A2%E3%83%AD%E3%82%A8?src=hashtag">#Globalアロエ</a> on Twitter, or visit <a href="https://manoaloe.jetri.co/">dragonjet</a>'s site for the rest!
+										</div>
 									</div>
 								</div>
             </div>
