b985a05361727c0ed2db6393e4feade5ced60067
tduong <duongt.sbc@gmail.com>
2020-08-31 13:36:41 -0700

    revision of translated message

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -55,7 +55,7 @@ export default class App extends React.Component<AppProps, LanguageContextValue>
                                         Dear Aloe, thank you for everything. While it may have been but for a short while, we appreciate every little thing you've given us. We wish you the best of luck going forward.
                                     </p>
                                     <p>
-                                        親愛なるアロエ様、いろいろありがとうございます。短い間ではありましたが、一つ一つのことに感謝しています。今後のご活躍をお祈りしております。
+                                         親愛なるアロエ様。短い間ではありましたが、一緒に過ごした時間に心より感謝しております。本当にありがとうございました。アロエ様のご健勝とご多幸をお祈り申し上げます。
                                     </p>
                                 </div>
                             </div>
