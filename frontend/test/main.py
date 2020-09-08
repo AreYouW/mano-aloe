7bf9caef6dde308aedbc1bc3e2089f3e65e36468
dr-Jess <jessej.zong@gmail.com>
2020-09-08 18:02:43 -0400

    Currently the arg parse thing for the Chromedriver path is broken, save me from my incompetence please.

--- frontend/test/main.py
+++ frontend/test/main.py
@@ -3,7 +3,15 @@ import requests
 import json
 import time
 import pdb
-driver = webdriver.Chrome("/users/Jesse/chromedriver")
+import argparse
+parser = argparse.ArgumentParser(
+        description='Currently being used to parse chromedriver path')
+parser.add_argument(
+     '--chromedriver_path', '-c', dest='chromedriver_path',
+     default = '/chromedriver.exe',
+     help = 'the path to your chromedriver')
+args = parser.parse_args()
+driver = webdriver.Chrome(args.chromedriver_path)
 driver.implicitly_wait(5)
 driver.get("https://manotomo.tk")
 api = requests.get("https://manotomo.tk/api/messages")
@@ -12,9 +20,11 @@ print(json.dumps(api_json, sort_keys=True, ensure_ascii=True, indent=4))
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 msg_card_head = '//div[@id="root"]/main/div[2]/div[2]/div[5]/div/div['
 msg_card_tail = ']/div[1]/div[@class="message-card-text active-message"]/div'
-msg_card_tail_jp = ']/div[1]/div[@class="message-card-text"]/div'
+flag_head = '//*[@id="root"]/main/div[2]/div[2]/div[5]/div/div['
+flag_tail = ']/div[2]/span/img'
 native_message_list = []
 jp_message_list = []
+flag_list = []
 DOM_list = []
 for message in api_json["messages"]:
     #pdb.set_trace()
@@ -23,6 +33,12 @@ for message in api_json["messages"]:
         api_json["messages"].index(message) + 1,
         msg_card_tail)
     driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_xpath(native_msg_xpath))
+    while 1 == 1:
+        if driver.find_element_by_xpath(native_msg_xpath).is_displayed():
+            jp_message_list.append(driver.find_element_by_xpath(native_msg_xpath).text)
+            break
+        else:
+            pass
     native_message_list.append(driver.find_element_by_xpath(native_msg_xpath).text)
 driver.find_element_by_xpath('//*[@id="root"]/main/header/div[2]/button').click()
 for message in api_json["messages"]:
@@ -30,7 +46,12 @@ for message in api_json["messages"]:
     jp_msg_xpath = "{}{}{}".format(
         msg_card_head,
         api_json["messages"].index(message) + 1,
-        msg_card_tail_jp)
+        msg_card_tail)
+    flag_xpath = "{}{}{}".format(
+        flag_head,
+        api_json["messages"].index(message) + 1,
+        flag_tail
+    )
     if api_json["messages"][api_json["messages"].index(message)]["tl_msg"] == "":
         jp_message_list.append("")
     else:
@@ -38,13 +59,21 @@ for message in api_json["messages"]:
         while 1 == 1:
             if driver.find_element_by_xpath(jp_msg_xpath).is_displayed():
                jp_message_list.append(driver.find_element_by_xpath(jp_msg_xpath).text)
-               print("BREAK")
                break
             else:
-                print("SLEPT")
-
+                pass
+    if api_json["messages"][api_json["messages"].index(message)]["country"] == "":
+        flag_list.append("")
+    else:
+        driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_xpath(flag_xpath))
+        while 1 == 1:
+            if driver.find_element_by_xpath(flag_xpath).is_displayed():
+                flag_list.append(driver.find_element_by_xpath(flag_xpath).get_attribute("alt"))
+                break
+            else:
+                pass
     driver.execute_script("arguments[0].scrollIntoView();", driver.find_element_by_xpath(jp_msg_xpath))
-    DOM_list.append({"country": str(driver.find_elements_by_class_name("undefined")[api_json["messages"].index(message)].get_attribute("alt")),
+    DOM_list.append({"country": str(flag_list[api_json["messages"].index(message)]),
                      "orig_msg": str(native_message_list[api_json["messages"].index(message)]),
                      "tl_msg": str(jp_message_list[api_json["messages"].index(message)]),
                      "username": str(driver.find_elements_by_class_name("message-card-footer")[api_json["messages"].index(message)].text)})
