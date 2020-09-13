75f99b958ee857727984298f74c97956388874ea
Zapple <zizibaluba@gmail.com>
2020-09-12 19:53:28 -0700

    add opacity for active and inactive full page translation

--- frontend/src/components/languageSwitch/languageSwitch.tsx
+++ frontend/src/components/languageSwitch/languageSwitch.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {LanguageContext, LanguageContextValue} from "./languageContext";
+import DisplayedLanguage from '../../models/language';
 
 import IconButton from '@material-ui/core/IconButton';
 import TranslateIcon from '@material-ui/icons/Translate';
 
+var buttonStyleActive = {width: 64, height: 64, padding: 0, color: '#ffffff'}
+var buttonStyleInactive = {width: 64, height: 64, padding: 0, color: '#ffffff', opacity: '40%'}
+
 export default function LanguageSwitchButton() {
     return (
         <LanguageContext.Consumer>
-            {(value: LanguageContextValue) => 
+            {(value: LanguageContextValue) =>
                 <IconButton
-                    style={{width: 64, height: 64, padding: 0, color: '#ffffff'}}
-                    aria-label='Language Switch' onClick={value.toggleLanguage}>
+                        style={
+                            value.language == DisplayedLanguage.Original ?
+                                buttonStyleInactive : buttonStyleActive
+                        }
+                        aria-label='Language Switch'
+                        onClick={value.toggleLanguage}>
                     <TranslateIcon/>
                 </IconButton>
             }
