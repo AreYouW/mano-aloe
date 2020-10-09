7cb9431563862b0a680c555a867f9fc7dfb50bb4
tduong <duongt.sbc@gmail.com>
2020-10-08 21:43:38 -0700

    remove useless component

--- frontend/src/models/dummyMessage.ts
+++
@@ -1,8 +0,0 @@
-import {Region} from "./region";
-
-export interface MessageData {
-    Native_message: string;
-    JP_message_Deepl: string | null;
-    Name: string | null;
-    Region: Region | null; // TODO
-}
