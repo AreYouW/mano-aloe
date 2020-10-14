d6ef7f037424438462c3a868188b084f989f6193
tduong <duongt.sbc@gmail.com>
2020-10-14 13:18:40 -0700

    remove fade component

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -5,8 +5,6 @@ import {Message} from "../../models/message";
 import {toRegion} from "../../models/region";
 import ManoAloeService from "../../controllers/mano-aloe.service";
 import SessionService from "../../services/session.service";
-import Spinner from "../../shared/components/spinner/spinner";
-import Fade from "../../shared/animation/fade";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import ArrowDropDownCircleOutlinedIcon from '@material-ui/icons/ArrowDropDownCircleOutlined';
 import {Announcement} from "../../models/announcement"
@@ -84,7 +82,6 @@ export default class HomePage extends React.Component<HomePageProps, HomePageSta
         return (
             <section id='anchor'>
                 <div className="home-root">
-                    <Fade mounted={this.state.loading} childComponent={<Spinner/>}/>
                     <ArchiveSection />
                     <div className="justify-center padding-top">
                         <div className="justify-align-center">
