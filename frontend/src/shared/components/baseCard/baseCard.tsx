2d39e6fa76ef04e311d1d82b3eee1e894c1ef193
tduong <duongt.sbc@gmail.com>
2020-10-16 03:49:20 -0700

    hotfix css for production

--- frontend/src/shared/components/baseCard/baseCard.tsx
+++ frontend/src/shared/components/baseCard/baseCard.tsx
@@ -3,7 +3,7 @@ import DisplayedLanguage from "../../../models/language";
 import CSS from "csstype";
 // Throwing in a fake card style in until we have actual cards
 import CardStyle1 from "../../../assets/cards/coco_card_2.svg";
-import CardStyle2 from "../../../assets/cards/card1.svg";
+import CardStyle2 from "../../../assets/cards/coco_card_2.svg";
 import './baseCard.css';
 import handleViewport from "react-in-viewport";
 import VisibilitySensor from "react-visibility-sensor";
