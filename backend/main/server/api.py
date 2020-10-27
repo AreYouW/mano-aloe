f8ad886f7850313704543450cd60a1692d6bf8cb
tduong <duongt.sbc@gmail.com>
2020-10-26 20:47:57 -0700

    remove unused api endpoints

--- backend/main/server/api.py
+++ backend/main/server/api.py
@@ -4,9 +4,7 @@ from flask_restful import Api
 from main.server.resources.Message import MessageListResource, MessageResource, MessageListRangeResource, MessageCount
 from main.server.resources.Game import GameCount, GameListResource
 from main.server.resources.Gallery import GalleryCount, GalleryListResource
-from main.server.resources.Archive import ArchiveListResource, ArchiveResource, ArchiveCount, ArchiveRandomResource
 from main.server.resources.Announcement import AnnouncementListResource, AnnouncementCount
-from main.server.resources.Animation import AnimationCount, AnimationListResource
 
 # Api Blueprint
 api_bp = Blueprint('api', __name__)
@@ -28,16 +26,6 @@ api.add_resource(GameCount, '/games/count')
 api.add_resource(GalleryListResource, '/gallery')
 api.add_resource(GalleryCount, '/gallery/count')
 
-# Archive
-api.add_resource(ArchiveListResource, '/archives/<who>')
-api.add_resource(ArchiveRandomResource, '/archives/<who>/random')
-api.add_resource(ArchiveResource, '/archives/<who>/<archiveID>')
-api.add_resource(ArchiveCount, '/archives/<who>/count')
-
 # Announcements
 api.add_resource(AnnouncementListResource, '/announcements')
 api.add_resource(AnnouncementCount, '/announcements/count')
-
-# Animations
-api.add_resource(AnimationListResource, '/animations')
-api.add_resource(AnimationCount, '/animations/count')
